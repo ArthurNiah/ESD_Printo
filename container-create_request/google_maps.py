aed59249fba6bcbc89f22813667d81deb87a3fa6
Arthur Hain <arthur.hain.2020@scis.smu.edu.sg>
2022-03-19 15:18:05 +0800

    Updated Googlemaps to return and handle errors

--- container-create_request/google_maps.py
+++ container-create_request/google_maps.py
@@ -6,48 +6,43 @@ import requests
 from invokes import invoke_http
 
 app = Flask(__name__)
-CORS(app)
 
 API_KEY = 'AIzaSyA2wZe6-CgWyeoEJha5aeZLQp-cpn4kb8k'
 
-# user_lat, user_long = map_client.geolocate()['location']['lat'], map_client.geolocate()['location']['lng']
-# url1 =f"https://maps.googleapis.com/maps/api/geocode/json?latlng={user_lat},{user_long}&key={API_KEY}"
-
-# # user_location = invoke_http(url, method="GET")
-# user_location = requests.request('GET', url2)
-# print(user_location.json()['results'][0]['formatted_address'])
-# print(url)
-# print(user_location['results'])
-
-
 #WORKS
-@app.route("/get_current_location", methods=['POST'])
-def get_current_location(user_location):
+@app.route("/get_current_location", methods=['GET'])
+def get_current_location(user_location="NUS"):
 
     try:
 
         map_client = googlemaps.Client(API_KEY)
-        temp_place_id = map_client.geocode(user_location)[0]['place_id']
+        response = map_client.geocode(user_location)
 
-        #Reverse Geocoding, Takes the place id and returns a location
-        url2 = f"https://maps.googleapis.com/maps/api/geocode/json?place_id={temp_place_id}&key={API_KEY}"
-        response = invoke_http(url2, method="GET")
-
-        if not response:
+        if response == []:
             return jsonify(
                 {
                     "code": 404, 
                     "data": user_location,
-                    "message": "Location was not found. Please try again!"
+                    "message": "Location was not valid. Please try again!"
                 }, 
             ), 404
 
         #return the location
         else:
+            #Getting a placeID to be used for storage
+            place_id = response[0]['place_id']
+            #Reverse Geocoding, Takes the place id and returns a location
+            url2 = f"https://maps.googleapis.com/maps/api/geocode/json?place_id={place_id}&key={API_KEY}"
+            rev_geoencode = invoke_http(url2, method="GET")
+            formatted_address = rev_geoencode['results'][0]['formatted_address']
             return jsonify(
                 {
                     "code": 201, 
-                    "data": response['results'][0]['formatted_address'],
+                    "data": {
+                        "user_input": user_location,
+                        "placeID" : place_id, 
+                        "location" : formatted_address
+                    },
                 }
             ), 201
 
@@ -62,10 +57,5 @@ def get_current_location(user_location):
             },
         ), 500
 
-# map_client = googlemaps.Client(API_KEY)
-# temp_place_id = map_client.geocode('NUS')[0]['place_id']
-
-# #Reverse Geocoding, Takes the place id and returns a location
-# url2 = f"https://maps.googleapis.com/maps/api/geocode/json?place_id={temp_place_id}&key={API_KEY}"
-# response = invoke_http(url2, method="GET")
-# print(response['results'][0]['formatted_address'])
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
