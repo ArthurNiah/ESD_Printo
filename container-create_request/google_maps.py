a491ced5b1b553dabc861e7bf12821accd22a320
Arthur Hain <arthur.hain.2020@scis.smu.edu.sg>
2022-03-19 15:54:58 +0800

    added lat,lng to response

--- container-create_request/google_maps.py
+++ container-create_request/google_maps.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 API_KEY = 'AIzaSyA2wZe6-CgWyeoEJha5aeZLQp-cpn4kb8k'
 
-#WORKS
-@app.route("/get_current_location")
+# #WORKS
+@app.route("/get_current_location", methods=['GET'])
 def get_current_location(user_location="NUS"):
 
     try:
@@ -31,17 +31,22 @@ def get_current_location(user_location="NUS"):
         else:
             #Getting a placeID to be used for storage
             place_id = response[0]['place_id']
+            print(place_id)
             #Reverse Geocoding, Takes the place id and returns a location
             url2 = f"https://maps.googleapis.com/maps/api/geocode/json?place_id={place_id}&key={API_KEY}"
             rev_geoencode = invoke_http(url2, method="GET")
             formatted_address = rev_geoencode['results'][0]['formatted_address']
+            user_lat = response[0]['geometry']['location']['lat']
+            user_lng = response[0]['geometry']['location']['lng']
             return jsonify(
                 {
                     "code": 201, 
                     "data": {
                         "user_input": user_location,
                         "placeID" : place_id, 
-                        "location" : formatted_address
+                        "locationName" : formatted_address,
+                        "lat": user_lat, 
+                        "lng": user_lng
                     },
                 }
             ), 201
@@ -59,3 +64,11 @@ def get_current_location(user_location="NUS"):
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+
+
+
+#FOR DEBUGGING PURPOSES (REMOVE AT THE END OF PROJECT!!!)
+# map_client = googlemaps.Client(API_KEY)
+# response = map_client.geocode("NUS")
+# print(response[0]['geometry']['location']['lat'])
+# print(response[0]['geometry']['location']['lng'])
