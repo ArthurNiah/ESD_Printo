3e1af9ae68554251b3c14d55479d8397909ad7bc
JinLiang Loh <jlloh.2020@scis.smu.edu.sg>
2022-03-24 17:52:17 +0800

    create_provider uploaded

---
+++ container-create_provider/create_provider.py
@@ -0,0 +1,112 @@
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os, sys
+import requests
+
+
+# import amqp_setup
+import pika
+import json
+
+#import internal files
+from invokes import invoke_http
+from google_maps import get_current_location
+
+app = Flask(__name__)
+CORS(app)
+
+
+provider_URL = "http://localhost:5007/provider" #take note which localhost number you using and calling
+googleDrive_URL = ""
+googleMaps_URL = "http://localhost:5002/get_current_location" #take note which localhost number you using and calling
+requestDB_URL = ""
+
+#Info from UI
+user_input = {}
+
+
+@app.route("/create_provider", methods=["POST"])
+#For creating a new requestd
+def create_provider():
+
+    if request.is_json:
+        try:
+            provider_details = request.get_json()
+            print("\Created provider details in JSON:", provider_details)
+
+            # do the actual work
+            # 1. Send order info {cart items}
+            result = processProvider(provider_details)
+            return jsonify(result), result["code"]
+
+        except Exception as e:
+            # Unexpected error in code
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            ex_str = str(e) + " at " + str(exc_type) + ": " + fname + ": line " + str(exc_tb.tb_lineno)
+            print(ex_str)
+
+            return jsonify({
+                "code": 500,
+                "message": "create_provider.py internal error: " + ex_str
+            }), 500
+
+    # if reached here, not a JSON request.
+    return jsonify({
+        "code": 400,
+        "message": "Invalid JSON input: " + str(request.get_data())
+    }), 400
+
+#process the data(adpated from the sldies)
+def processProvider(provider_details):
+
+    #Step 1: Invoking Google Maps microservice
+    print('\n-----Invoking GoogleMaps microservice-----')
+    locationResults = invoke_http(googleMaps_URL, method="GET", json=provider_details) #Replace with variable 
+    print("Current provider location:", locationResults)
+
+    #====START: Error handeling for Google Maps API======
+    if locationResults['code'] not in range(200, 300):
+
+        # Inform the error microservice
+        print('\n\n-----Google Maps API microservice has failed-----')
+
+        #Return error to UI???
+        return {
+            "code": 500,
+            "message": "Google Maps Microservice API has failed. Please read error message and try again.",
+            "errorMsg": locationResults['message']
+        }
+    #====END: Error handeling for Google Maps API======
+
+
+    #Step 2: Invoking Provider microservice (Update to be done in the Provider Microservice)
+    print('\n-----Invoking Provider microservice-----')
+    # Need to find a way to add new data into a json file
+    print(provider_details)
+    provider_results = invoke_http(provider_URL, method="POST", json=provider_details)
+
+    #====START: Error handeling for Provider Microservice======
+    if provider_results['code'] == 500:
+
+        #Return error to UI???
+        return {
+            "code": 500,
+            "message": "An error occurred in creating the Provider.",
+        }
+    #====END: Error handeling for Provider Microservice======
+
+    return{
+        "code": 201,
+        "data": {
+            "message": "Provider details was added successfully!"
+        }
+    }
+
+
+
+
+if __name__ == "__main__":
+    print("This is flask " + os.path.basename(__file__) + " for placing an request...")
+    app.run(port=5006, debug=True)
---
+++ container-create_provider/provider.py
@@ -0,0 +1,106 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS 
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/provider'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ 
+db = SQLAlchemy(app)
+CORS(app)
+ 
+class Provider(db.Model):
+    __tablename__ = 'provider'
+ 
+    provider_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), nullable=False)
+    tele_id = db.Column(db.String(64), nullable=False)
+    coordinates = db.Column(db.String(64), nullable=False)
+    location_name = db.Column(db.String(64), nullable=False)
+ 
+    def __init__(self, username, tele_id, coordinates, location_name):
+        self.username = username
+        self.tele_id = tele_id
+        self.coordinates = coordinates
+        self.location_name = location_name
+
+ 
+    def json(self):
+        return {
+        "username": self.username, 
+        "tele_id": self.tele_id,
+        "coordinates": self.coordinates,
+        "location_name": self.location_name,
+        }
+ 
+@app.route("/provider")
+def get_all():
+    providerlist = Provider.query.all()
+    if len(providerlist):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "providers": [provider.json() for provider in providerlist]
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There are no providers."
+        }
+    ), 404
+ 
+@app.route("/provider/<string:provider_id>")
+def find_by_provider_id(provider_id):
+    provider = Provider.query.filter_by(provider_id=provider_id).first()
+    if provider:
+        return jsonify(
+            {
+                "code": 200,
+                "data": provider.json()
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "Provider not found."
+        }
+    ), 404
+ 
+@app.route("/provider", methods=['POST'])
+def insert_provider():
+
+    data = request.get_json()
+    provider = Provider(**data)
+
+    # username = request.json.get('username', None)
+    # tele_id = request.json.get('tele_id', None)
+    # coordinates = request.json.get('coordinates', None)
+    # location_name = request.json.get('location_name', None)
+
+    # provider = Provider(username=username, tele_id=tele_id, coordinates=coordinates, location_name=location_name)
+ 
+    try:
+        db.session.add(provider)
+        db.session.commit()
+    except:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "An error occurred creating the Provider."
+            }
+        ), 500
+ 
+    return jsonify(
+        {
+            "code": 201,
+            "data": provider.json()
+        }
+    ), 201
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5007, debug=True)  
