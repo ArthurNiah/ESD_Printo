f5f532ef3b19266c4d58925895978253b1c04263
Arthur Hain <arthur.hain.2020@scis.smu.edu.sg>
2022-04-02 17:38:00 +0800

    updated payment id

---
+++ container-accept_request/payment.py
@@ -0,0 +1,64 @@
+from glob import escape
+from http.client import REQUEST_ENTITY_TOO_LARGE
+from flask import Flask, request as req, jsonify
+from flask_cors import CORS
+import os, sys
+import requests
+
+
+# import amqp_setup
+import pika
+import json
+
+#import internal files
+from invokes import invoke_http
+# from google_maps import get_current_location
+
+app = Flask(__name__)
+CORS(app)
+
+
+price_list = {
+    "A4": 1.0, 
+    "A3": 1.0, 
+    "non_color" : 1.0, 
+    "color" : 1.0, 
+    "base_price" : 2.00, 
+    "copy_price" : 1.00
+}
+
+
+@app.route("/payment", methods=["GET"])
+def cal_payment():
+
+    request = req.get_json()
+    if request:
+        try:
+            color = request['color']
+            copies = int(request['no_of_copies'])
+            size = request['size']
+
+            final_price = price_list['base_price'] + price_list[color] + price_list[size] + copies*price_list['copy_price']
+            return jsonify (
+                {
+                    "code": 200, 
+                    "message": "Final price microservice success", 
+                    "data": {
+                        "final_price":final_price, 
+                        "response": request.json()
+                    }
+                }
+            ), 200
+
+        except Exception as e:
+
+            return jsonify({
+                "code": 500, 
+                "message": "Final price microservice failed", 
+                "data": {
+                    "response":request.json()
+                }
+            }), 500
+
+
+    return "hello"
