472822592e7fc7f78c983b20b865e66739e14b5a
sabrinahallmi <sabrinah.2020@scis.smu.edu.sg>
2022-04-02 22:29:20 +0800

    updated get_provider_location.py

--- container-view_request/view_request/get_provider_location.py
+++ container-view_request/view_request/get_provider_location.py
@@ -8,8 +8,6 @@ from flask_sqlalchemy import SQLAlchemy
 # for containerization later on
 from os import environ
 
-# api key (Sabbie's)
-api_key = 'AIzaSyAtQumxZP0XtDgLgYSV8Fcb8heVm5VRlJE'
 # ---------------------------------------------------------------------------------------------------------------- #
 app = Flask(__name__)
 
@@ -33,33 +31,57 @@ class Provider(db.Model):
     provider_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), nullable=False)
     tele_id = db.Column(db.String(64), nullable=False)
-    coordinates = db.Column(db.String(64), nullable=False)
+    lat = db.Column(db.String(64), nullable=False)
+    lng = db.Column(db.String(64), nullable=False)
     location_name = db.Column(db.String(64), nullable=False)
+    place_id = db.Column(db.String(64), nullable=False)
+    chat_id = db.Column(db.String(64), nullable=False)
+    first_name = db.Column(db.String(64), nullable=False)
+    last_name = db.Column(db.String(64), nullable=False)
  
-    def __init__(self, username, tele_id, coordinates, location_name):
+    def __init__(self, username, tele_id, lat, lng, location_name, place_id, chat_id, first_name, last_name):
         self.username = username
         self.tele_id = tele_id
-        self.coordinates = coordinates
+        self.lat = lat
+        self.lng = lng
         self.location_name = location_name
+        self.place_id = place_id
+        self.chat_id = chat_id
+        self.first_name = first_name
+        self.last_name = last_name
 
  
     def json(self):
         return {
         "username": self.username, 
         "tele_id": self.tele_id,
-        "coordinates": self.coordinates,
+        "lat": self.lat,
+        "lng": self.lng,
         "location_name": self.location_name,
+        "place_id": self.place_id,
+        "chat_id": self.chat_id,
+        "first_name": self.first_name,
+        "last_name": self.last_name
         }
 
-# retrieve provider based on provider_id
 @app.route("/get_provider_location/<string:provider_id>")
-def find_by_tele_id(provider_id):
+def get_provider_location(provider_id):
 
     provider = Provider.query.filter_by(provider_id=provider_id).first()
-
-    if provider:
+    
+    # if provider:
         # change to place_id later
-        return provider.location_name
+        # return provider.place_id
+    
+    if provider:
+        return jsonify(
+            {
+                "code": 200,
+                "place_id": "place_id:"+provider.place_id
+
+            }
+        )
+        
     
     return jsonify(
         {
