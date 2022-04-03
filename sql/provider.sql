fb3ed8121fb082a965dfe9023b32c4457bc7aa51
JinLiang Loh <jlloh.2020@scis.smu.edu.sg>
2022-04-03 14:34:49 +0800

    provider sql update
    changed lat lng to coordinates

--- sql/provider.sql
+++ sql/provider.sql
@@ -35,9 +35,8 @@ CREATE TABLE IF NOT EXISTS `provider` (
   `provider_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(64) NOT NULL,
   `tele_id` varchar(64) NOT NULL,
-  `lat` varchar(64) NOT NULL,
-  `lng` varchar(64) NOT NULL,
-  `location_name` varchar(64) NOT NULL,
+  `coordinates` varchar(64) NOT NULL,
+  `location` varchar(64) NOT NULL,
   `place_id` varchar(64) NOT NULL,
   `chat_id` varchar(64) NOT NULL,
   `first_name` varchar(64) NOT NULL, 
@@ -49,10 +48,10 @@ CREATE TABLE IF NOT EXISTS `provider` (
 -- Dumping data for table `provider`
 --
 
-INSERT INTO `provider` (`provider_id`, `username`, `first_name`, `last_name`, `tele_id`, `lat`, `lng`, `location_name`, `place_id`, `chat_id`) VALUES
-('1', 'arthur', 'Arthur', 'Hain', '@arthur', '550', '400','smu', 'tochange', '853733285'),
-('2', 'julian', 'Julian', 'Ung', '@julian', '550', '400','bedok', 'tochange', '853733285'),
-('3', 'joseph', 'Joseph', 'Alde', '@joseph', '550', '400','bencoolen', 'tochange',' 853733285');
+INSERT INTO `provider` (`provider_id`, `username`, `first_name`, `last_name`, `tele_id`, `coordinates`, `location`, `place_id`, `chat_id`) VALUES
+('1', 'arthur', 'Arthur', 'Hain', '@arthur', '550 400', 'smu', 'tochange', '853733285'),
+('2', 'julian', 'Julian', 'Ung', '@julian', '550 400', 'bedok', 'tochange', '853733285'),
+('3', 'joseph', 'Joseph', 'Alde', '@joseph', '550 400', 'bencoolen', 'tochange',' 853733285');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
