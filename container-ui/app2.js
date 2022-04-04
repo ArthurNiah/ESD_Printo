d5e1735ddf599d14174f0d8d5a10f4b2a70a5946
zenyu1 <ongzenyu112@gmail.com>
2022-04-05 04:48:52 +0800

    commit

--- container-ui/app2.js
+++ container-ui/app2.js
@@ -1,5 +1,6 @@
 const express= require('express')
 const axios= require('axios')
+const formidable= require('formidable')
 const app= express()
 var fs = require('fs');
 
@@ -154,57 +155,49 @@ app.get('/fileuploadui', (req,res)=>{
 
 app.post('/fileupload',(req,res)=>{
     console.log(req.body)
-    // form.parse(req, function(err, fields, files){
-    //     console.log('2')
-    //     file_name= files.filetoupload.originalFilename;
-    //     mime_type= files.filetoupload.mimetype
-    //     var info= JSON.stringify({
-    //     'location': fields.location, 'requestor_id': fields.requestor_id, 'no_of_copies': fields.no_of_copies, 'color': fields.color, 'size': fields.size, 'single_or_double': fields.single_or_double, 
-    //     'comments': fields.comments, 'file_name': file_name, 'mime_type': mime_type});
+    var form= new formidable.IncomingForm();
 
-    //     console.log(info)
-    // })
-        // const options = {
-        //   hostname: 'localhost',
-        //   port: 5001,
-        //   path: '/create_request',
-        //   method: 'POST',
-        //   headers: {
-        //     'Content-Type': 'application/json',
-        //     'Content-Length': info.length
-        //   }
-        // }   
-  
-        // const req = http.request(options, res =>{
-        //   console.log('statusCode:', res.StatusCode)
-        //   console.log('headers:', res.headers)
-        //   res.on('info', d => {
-        //     process.stdout.write(d)
-        //   })
-        // }); 
-  
-        // req.on('error', error => {
-        //   console.error(error)
-        // })
+    form.parse(req, function(err, fields, files){
+        console.log(files)
+        console.log('2')
+        file_name= files.filetoupload.originalFilename;
+        mime_type= files.filetoupload.mimetype
+        var info= JSON.stringify({
+        'location': fields.location, 'requestor_id': fields.requestor_id, 'no_of_copies': fields.no_of_copies, 'color': fields.color, 'size': fields.size, 'single_or_double': fields.single_or_double, 
+        'comments': fields.comments, 'file_name': file_name, 'mime_type': mime_type});
+
+        console.log(info)
+
+        axios({
+            method: 'post',
+            url: 'http://localhost:5001/create_request',
+            data: {
+                'location': fields.location, 'requestor_id': fields.requestor_id, 'no_of_copies': fields.no_of_copies, 'color': fields.color, 'size': fields.size, 'single_or_double': fields.single_or_double, 
+                'comments': fields.comments, 'file_name': file_name, 'mime_type': mime_type}
+        })
+
+    
+        .then((response)=>{
+            console.log(response)
+        })
+    
+
+        var dir= __dirname + "/temp_files"
+        if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+        }
         
-        // req.write(info)
-  
-        // var dir= __dirname + "/temp_files"
-        // if (!fs.existsSync(dir)){
-        //   fs.mkdirSync(dir);
-        // }
-        
-        // var oldpath = files.filetoupload.filepath;
-        // var newpath = dir + "/" + file_name;
-        // fs.rename(oldpath, newpath, function (err) {
-        //   if (err) throw err;
-        //   res.write('Your request has been posted!');
-        //   res.end();
-        // });
+        var oldpath = files.filetoupload.filepath;
+        var newpath = dir + "/" + file_name;
+        fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('Your request has been posted!');
+        res.end();
+        });
     return res.end()
 })
 
-
+})
 app.listen(3030, ()=>{
     console.log('Server started at http:localhost:3030')
 })
